ea8748c8317f161dd669492e5e6e0e2d7668ee67
saravmani <saravmanicloud@gmail.com>
2022-11-09 06:37:38 +0530

    ODATA

--- Controllers/StudentController.cs
+++ Controllers/StudentController.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.OData.Query;
 using Scorpion.Template.AspNet_EfCore.Model;
 using Scorpion.Template.AspNet_EfCore.Repository;
 
@@ -12,18 +13,19 @@ namespace Scorpion.Template.AspNet_EfCore.Controllers
         private readonly ILogger<StudentController> _logger;
         private readonly ApplicationDbContext dbContext;
 
-        //public StudentController(ILogger<StudentController> logger, ApplicationDbContext dbContext)
-        //{
-        //    _logger = logger;
-        //    this.dbContext = dbContext;
-        //}
+        public StudentController(ILogger<StudentController> logger, ApplicationDbContext dbContext)
+        {
+            _logger = logger;
+            this.dbContext = dbContext;
+        }
 
-        //[HttpGet(Name = "GetStudentList")]
-        //public IEnumerable<Student> GetStudentList()
-        //{
-        //    return dbContext.Students.ToList();
-            
-        //}
+        [HttpGet(Name = "GetStudentList")]
+        [EnableQuery]
+        public IQueryable<Student> GetStudentList()
+        {
+            return dbContext.Students.AsQueryable();
+
+        }
 
         //[HttpPost(Name = "AddStudents")]
         //public IActionResult AddStudents([FromBody] List<Student> studentsList)
--- Program.cs
+++ Program.cs
@@ -17,60 +17,38 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllers();
-//builder.Services.AddControllers().AddOData(opt => opt.AddRouteComponents("odata", GetEdmModel()));
+builder.Services.AddControllers().AddOData(opt =>
+{
+  
+    opt.Select();
+
+}
+);
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-//builder.Services.AddDbContext<ApplicationDbContext>();
-builder.Services.AddPooledDbContextFactory<ApplicationDbContext>((options) => {
+
+
+builder.Services.AddDbContext<ApplicationDbContext>((options) =>
+{
     options.LogTo(Console.WriteLine);
     options.UseSqlite("Data Source=ApplicaitonDb.db");
 });
 
 
-
-//builder.Services
-//    .AddGraphQL((builder) =>
-//    {
-//        builder.ConfigureExecutionOptions(opt =>
-//        {
-//            opt.EnableMetrics = true;
-
-//            //opt.UnhandledExceptionDelegate = ctx => logger.LogError("{Error} occurred", ctx.OriginalException.Message);
-//        });
-//        builder.AddGraphTypes().AddSystemTextJson();
+//Used by graphQL
+//builder.Services.AddPooledDbContextFactory<ApplicationDbContext>((options) => {
+//    options.LogTo(Console.WriteLine);
+//    options.UseSqlite("Data Source=ApplicaitonDb.db");
+//});
 
 
-//    });
 
+
+//Used by graphQL
 builder.Services.AddGraphQLServer()
-    .AddQueryType<Scorpion.Template.AspNet_EfCore.Repository.Query>()    
-    .AddFiltering()
-    
-    ;
-    
-
-
-//(options, provider) =>
-//{
-//            // Load GraphQL Server configurations
-//            var graphQLOptions = Configuration
-//        .GetSection("GraphQL")
-//        .Get<GraphQLOptions>();
-//    options.ComplexityConfiguration = graphQLOptions.ComplexityConfiguration;
-//    options.EnableMetrics = graphQLOptions.EnableMetrics;
-//            // Log errors
-//            var logger = provider.GetRequiredService<ILogger<Startup>>();
-//    options.UnhandledExceptionDelegate = ctx =>
-//        logger.LogError("{Error} occurred", ctx.OriginalException.Message);
-//})
-//    // Adds all graph types in the current assembly with a singleton lifetime.
-//    .AddGraphTypes()
-//    // Add GraphQL data loader to reduce the number of calls to our repository. https://graphql-dotnet.github.io/docs/guides/dataloader/
-//    .AddDataLoader()
-//    .AddSystemTextJson();
-
-
+    .AddQueryType<Scorpion.Template.AspNet_EfCore.Repository.Query>()
+    .AddFiltering();
 
 
 
@@ -85,23 +63,16 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-//GraphQL.Server.Ui.Altair.AltairOptions
-//app.UseAuthorization();
-//app.UseGraphQLAltair("ui/altair");
 app.UseRouting();
 
+//To Enable GraphQL Endpoint
 app.UseEndpoints(endpoints =>
 {
-    endpoints.MapGraphQL(path:"/graphql");
+    endpoints.MapGraphQL(path: "/graphql");
 });
 
 
 app.MapControllers();
 
 app.Run();
-
-
-//static IEdmModel GetEdmModel()
-//{
-//    return null;
-//}
+ 
